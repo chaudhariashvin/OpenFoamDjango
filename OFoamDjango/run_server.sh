aa5280f9cc8a5e32623c1f87b4c56691e4b0fab9
chaudhariashvin <38039585+chaudhariashvin@users.noreply.github.com>
2024-03-27 18:47:21 +0200

    Add files via upload

---
+++ OFoamDjango/run_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./run_display.sh
+
+source /opt/openfoam7/etc/bashrc
+
+git clone https://github.com/Franjcf/hybridPorousInterFoam.git
+cd hybridPorousInterFoam/OpenFoamV7/
+./Allwclean
+./Allwmake
+cd ../../
+python3 manage.py runserver 0.0.0.0:8000
